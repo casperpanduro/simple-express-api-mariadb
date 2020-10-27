93e0837c84d310bd503b6f3b4f1dce75df307e90
Casper Panduro <cap@checkmate.dk>
2020-10-27 13:25:59 +0100

    added db connection

---
+++ db.js
@@ -0,0 +1,24 @@
+// import mariadb
+var mariadb = require('mariadb');
+require('dotenv').config();
+
+// create a new connection pool
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+
+// expose the ability to create new connections
+module.exports={
+    getConnection: function(){
+      return new Promise(function(resolve,reject){
+        pool.getConnection().then(function(connection){
+          resolve(connection);
+        }).catch(function(error){
+          reject(error);
+        });
+      });
+    }
+  } 
